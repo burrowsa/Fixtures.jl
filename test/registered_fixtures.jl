f0e02446656b640a59a7d2119b5e5b615190778a
Andrew Burrows <burrowsa@gmail.com>
2014-01-20 11:10:55 +0000

    Tests for new fixture_values functionality

--- test/registered_fixtures.jl
+++ test/registered_fixtures.jl
@@ -228,6 +228,124 @@ facts("Registered Fixtures tests") do
     @fact x => 100
     @fact y => 200
   end
+
+  context("A fixture with no values", using_fixtures) do
+    function nadda(fn::Function)
+      fn()
+    end
+
+    add_fixture(:examplescope, :nowt, nadda)
+
+    apply_fixtures(:examplescope, fixture_values=true) do values
+      @fact collect(keys(values)) => [:nowt]
+      @fact values[:nowt] => nothing
+    end
+  end
+
+  context("A fixture with one value", using_fixtures) do
+    function produce_n(fn::Function, n::Integer)
+      fn(n)
+    end
+
+    add_fixture(:examplescope, :one_hundred, produce_n, 100)
+
+    apply_fixtures(:examplescope, fixture_values=true) do values
+      @fact collect(keys(values)) => [:one_hundred]
+      @fact values[:one_hundred] => 100
+    end
+  end
+
+  context("A fixture with one 0-tuple value", using_fixtures) do
+    function produce_empty(fn::Function)
+      fn(())
+    end
+
+    add_fixture(:examplescope, :empty, produce_empty)
+
+    apply_fixtures(:examplescope, fixture_values=true) do values
+      @fact collect(keys(values)) => [:empty]
+      @fact values[:empty] => ()
+    end
+  end
+
+  context("A fixture with one 1-tuple value", using_fixtures) do
+    function produce_n(fn::Function, n::Integer)
+      fn((n,))
+    end
+
+    add_fixture(:examplescope, :one_hundred, produce_n, 100)
+
+    apply_fixtures(:examplescope, fixture_values=true) do values
+      @fact collect(keys(values)) => [:one_hundred]
+      @fact values[:one_hundred] => (100,)
+    end
+  end
+
+  context("A fixture with one 2-tuple value", using_fixtures) do
+    function produce_n(fn::Function, n::Any)
+      fn(n)
+    end
+
+    add_fixture(:examplescope, :some_nums, produce_n, (100, 200))
+
+    apply_fixtures(:examplescope, fixture_values=true) do values
+      @fact collect(keys(values)) => [:some_nums]
+      @fact values[:some_nums] => (100, 200)
+    end
+  end
+
+  context("A fixture with two values", using_fixtures) do
+    function produce_n_and_m(fn::Function, n::Integer, m::Integer)
+      fn(n, m)
+    end
+
+    add_fixture(:examplescope, :some_nums, produce_n_and_m, 100, 200)
+
+    apply_fixtures(:examplescope, fixture_values=true) do values
+      @fact collect(keys(values)) => [:some_nums]
+      @fact values[:some_nums] => (100, 200)
+    end
+  end
+
+  context("A fixture with three values", using_fixtures) do
+    function produce_n_m_and_o(fn::Function, n::Integer, m::Integer, o::Integer)
+      fn(n, m, o)
+    end
+
+    add_fixture(:examplescope, :some_nums, produce_n_m_and_o, 100, 200, 300)
+
+    apply_fixtures(:examplescope, fixture_values=true) do values
+      @fact collect(keys(values)) => [:some_nums]
+      @fact values[:some_nums] => (100, 200, 300)
+    end
+  end
+
+  context("An anonymous fixture with one value", using_fixtures) do
+    function produce_n(fn::Function, n::Integer)
+      fn(n)
+    end
+
+    add_fixture(:examplescope, produce_n, 100)
+
+    apply_fixtures(:examplescope, fixture_values=true) do values
+      @fact collect(keys(values)) => []
+    end
+  end
+
+  context("Two fixture with one value each", using_fixtures) do
+    function produce_n(fn::Function, n::Integer)
+      fn(n)
+    end
+
+    add_fixture(:examplescope, :peter, produce_n, 100)
+    add_fixture(:examplescope, :paul, produce_n, 200)
+
+    apply_fixtures(:examplescope, fixture_values=true) do values
+      @fact sort(collect(keys(values))) => sort([:peter, :paul])
+      @fact values[:peter] => 100
+      @fact values[:paul] => 200
+    end
+  end
 end
 
 end
