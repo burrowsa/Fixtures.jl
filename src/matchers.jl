6ba1aceb7b6ac2c75ad02c1bf26da3cdbc49563b
Andrew Burrows <burrowsa@gmail.com>
2014-01-25 14:15:29 +0000

    Matcher should overload isequal not ==

--- src/matchers.jl
+++ src/matchers.jl
@@ -5,9 +5,11 @@ immutable Matcher
   description::String
 end
 
-==(lhs::Matcher, rhs::Matcher) = error("Can not compare 2 Matchers")
-==(matcher::Matcher, value::Any) = matcher.matches(value)
-==(value::Any, matcher::Matcher) = matcher.matches(value)
+Base.isequal(lhs::Matcher, rhs::Matcher) = error("Can not compare 2 Matchers")
+Base.isequal(matcher::Matcher, value::WeakRef) = matcher.matches(value)
+Base.isequal(matcher::Matcher, value::Any) = matcher.matches(value)
+Base.isequal(value::WeakRef, matcher::Matcher) = matcher.matches(value)
+Base.isequal(value::Any, matcher::Matcher) = matcher.matches(value)
 
 Base.show(io::IO, matcher::Matcher) = print(io, matcher.description)
 
